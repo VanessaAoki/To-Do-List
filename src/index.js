882f8b82e1a8bcd2a160a79eb282d4b18ecb1438
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-08-17 21:46:05 -0300

    Finish styling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
 import _ from 'lodash';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const myToDo = [
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 0
+  },
+  {
+    description: 'complete To Do list project',
+    completed: false,
+    index: 1
+  },
+  {
+    description: 'attend stand up meeting',
+    completed: false,
+    index: 2
+  }
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+function renderList() {
+  const list = document.getElementById('to-do-list');
+  list.innerHTML = '';
+  let index = 0;
+  for (const todo of myToDo) {
+    list.innerHTML += `
+    <li class="task-container">
+      <div class="checkbox-description-container">
+        <input type="checkbox">
+        <p>${todo.description}</p>
+      </div>
+      <i class="fas fa-ellipsis-v"></i>
+    </li>`;
+  }
 }
 
-document.body.appendChild(component());
+window.addEventListener('load', renderList);
